d72ab86df1093b302c0990a85c862d3a2dc4384e
ranand5 <hitanand4@gmail.com>
2018-08-21 18:22:02 +0530

    Adding the neural architecture of datasets

--- weather_forecasting.py
+++ weather_forecasting.py
@@ -9,7 +9,7 @@ import warnings
 
 
 warnings.filterwarnings("ignore")
-data = pd.read_csv("delhi-weather-data/testset.csv")
+data = pd.read_csv("testset.csv")
 #drop datetime variable
 data = data.drop(['datetime'], 1)
 
@@ -38,10 +38,8 @@ test_end = n
 temp_train = temperature[np.arange(train_start, train_end)]
 temp_test = temperature[np.arange(test_start, test_end)]
 
+print(temp_train)
 
-print(temp_train)
-temp_train1 = np.reshape(temp_train, (1, -1))
-print(temp_train)
 
 
 
@@ -68,7 +66,7 @@ y_test = temp_test[:]
 
 
 #Placeholder
-X = tf.placeholder(dtype=tf.float32 , shape=[None , 1])
+X = tf.placeholder(dtype=tf.float32 , shape=[100990 , 1])
 Y = tf.placeholder(dtype=tf.float32 , shape = [None] )
 
 n_classes = 10
@@ -77,11 +75,11 @@ batch_size = 100
 #Mode architecture parameters:
 #Three layer model
 n_temp = 1
-n_neuron_1 = 3
-n_neuron_2 = 3
-n_neuron_3 = 2
+n_neurons_1 = 3
+n_neurons_2 = 3
+n_neurons_3 = 2
 n_target = 1
-
+"""
 tf.set_random_seed(1)                   # so that your "random" numbers match ours
     
 ### START CODE HERE ### (approx. 6 lines of code)
@@ -95,10 +93,14 @@ b3 = tf.get_variable("b3", [2,1], initializer = tf.zeros_initializer())
 W_out = tf.get_variable("W_out", [1,3], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
 b_out = tf.Variable("b_out", [1,1])
 ### END CODE HERE ###
-
+"""
     
 
-'''
+# Initializers
+sigma = 1
+weight_initializer = tf.variance_scaling_initializer(mode="fan_avg", distribution="uniform", scale=sigma)
+bias_initializer = tf.zeros_initializer()
+
 # Layer 1: Variables for hidden weights and biases
 W1 = tf.Variable(weight_initializer([n_temp, n_neurons_1]))
 b1 = tf.Variable(bias_initializer([n_neurons_1]))
@@ -108,5 +110,73 @@ b2 = tf.Variable(bias_initializer([n_neurons_2]))
 # Layer 3: Variables for hidden weights and biases
 W3 = tf.Variable(weight_initializer([n_neurons_2, n_neurons_3]))
 b3 = tf.Variable(bias_initializer([n_neurons_3]))
-'''
 
+# Output layer: Variables for output weights and biases
+W_out = tf.Variable(weight_initializer([n_neurons_3, n_target]))
+bias_out = tf.Variable(bias_initializer([n_target]))
+
+# Hidden layer
+hidden_1 = tf.nn.relu(tf.add(tf.matmul(X, W1), b1))
+hidden_2 = tf.nn.relu(tf.add(tf.matmul(hidden_1, W2), b2))
+hidden_3 = tf.nn.relu(tf.add(tf.matmul(hidden_2, W3), b3))
+
+# Output layer (must be transposed)
+out = tf.transpose(tf.add(tf.matmul(hidden_3, W_out), bias_out))
+
+#cost function
+mse = tf.reduce_mean(tf.squared_difference(out, Y))
+#Optimizer
+opt = tf.train.AdamOptimizer().minimize(mse)
+
+# Make Session
+net = tf.Session()
+
+# Make Session
+net = tf.Session()
+
+# Run initializer
+net.run(tf.global_variables_initializer())
+
+# Setup interactive plot
+plt.ion()
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+line1, = ax1.plot(y_test)
+line2, = ax1.plot(y_test*0.5)
+plt.show()
+
+'''
+#Fitting the neural network
+# Number of epochs and batch size
+epochs = 10
+batch_size = 256
+
+for e in range(epochs):
+
+    # Shuffle training data
+    shuffle_indices = np.random.permutation(np.arange(len(y_train)))
+    X_train = X_train[shuffle_indices]
+    y_train = y_train[shuffle_indices]
+
+    # Minibatch training
+    for i in range(0, len(y_train) // batch_size):
+        start = i * batch_size
+        batch_x = X_train[start:start + batch_size]
+        batch_y = y_train[start:start + batch_size]
+        # Run optimizer with batch
+        net.run(opt, feed_dict={X: batch_x,Y: batch_y})
+
+        # Show progress
+        if np.mod(i, 5) == 0:
+            # Prediction
+            pred = net.run(out, feed_dict={X: X_test})
+            line2.set_ydata(pred)
+            plt.title('Epoch ' + str(e) + ', Batch ' + str(i))
+            file_name = 'img/epoch_' + str(e) + '_batch_' + str(i) + '.jpg'
+            plt.savefig(file_name)
+            plt.pause(0.01)
+# Print final MSE after Training
+
+mse_final = net.run(mse, feed_dict={X: X_test, Y: y_test})
+print(mse_final)
+'''
